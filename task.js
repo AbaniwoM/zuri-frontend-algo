4ffad3781b738d0d618000befc632f1c3eb83672
Your Name <you@example.com>
2021-05-19 22:23:08 +0100

    added to files

--- task.js
+++ task.js
@@ -1,5 +1,40 @@
-function convertFahrToCelsius () {
-    let f = document.getElementById("data").value;
-    let c = (f - 32) * 5/9;
-    document.getElementById("res").innerText = `${f} F = ${c} C`;
+function convertFahrToCelsius (f) {
+  let c = (f - 32) * 5/9;
+  let fahr = Object.prototype.toString.call(f);
+  if (fahr ==="[object Number]" || (fahr === "[object String]" && !isNaN(f))){
+  //Return your output here
+     return  c.toFixed(4);
+  }
+  else{
+       // put error code here
+       let paramtype = fahr.split(" ");
+       paramtype[1] = paramtype[1]. substring(0, paramtype[1]. length-1);
+       return `${JSON.stringify(f)} is not a valid number but a/an ${paramtype[1]}`;
+  }
+}
+
+function checkYuGiOh(n) {
+   let result =[];
+    if(isNaN(n)) {
+    return `${n} is not a valid number`;
+    }
+    for (let i = 0; i <= n; i++) {
+    let localvar=[];
+    if (i % 2  == 0) {
+     localvar.push("yu");
+    }
+    if (i % 3  == 0) {
+     localvar.push("gi");
+    }
+    if (i % 5  == 0) {
+     localvar.push("oh");
+    }
+    if (localvar.length > 0) {
+      result.push(localvar.join("-"));
+    }
+    else {
+      result.push(i);
+    }
+    }
+      return result;
 }
